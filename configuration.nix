4a5573da4714c0a9091252e54e5edb340f167541
Leonard Kinday <leonard@kinday.se>
2026-03-18 15:22:08 +0100

    Set home-manager version

--- configuration.nix
+++ configuration.nix
@@ -48,6 +48,7 @@
     { pkgs, config, ... }:
     {
       imports = [ inputs.quadlet-nix.homeManagerModules.quadlet ];
+
       virtualisation.quadlet.containers = {
         echo-server = {
           autoStart = true;
@@ -62,6 +63,15 @@
           };
         };
       };
+
+      # This value determines the Home Manager release that your configuration is
+      # compatible with. This helps avoid breakage when a new Home Manager release
+      # introduces backwards incompatible changes.
+      #
+      # You should not change this value, even if you update Home Manager. If you do
+      # want to update the value, then make sure to first check the Home Manager
+      # release notes.
+      home.stateVersion = "25.11"; # Please read the comment before changing.
     };
 
   system.stateVersion = "24.05";
