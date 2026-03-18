9dd8db1d0e740c12ef7d90a9de154c4170d3d96c
Leonard Kinday <leonard@kinday.se>
2026-03-18 15:45:33 +0100

    Use EFI bootloader

--- configuration.nix
+++ configuration.nix
@@ -12,12 +12,9 @@
     ./disk-configuration.nix
   ];
 
-  boot.loader.grub = {
-    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
-    # devices = [ ];
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
 
   services.openssh.enable = true;
 
